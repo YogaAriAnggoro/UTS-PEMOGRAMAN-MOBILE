8b661934eff7323866bfdfe7525461ef6255062c
YogaAriAnggoro <148923210+YogaAriAnggoro@users.noreply.github.com>
2025-04-08 20:37:06 +0700

    Update question.dart

--- models/question.dart
+++ models/question.dart
@@ -3,4 +3,8 @@ class Question {
   // Teks atau isi dari pertanyaan.
   final String questionText;
 
-  
+  // Jawaban benar dari pertanyaan, berupa nilai boolean (true atau false).
+  final bool correctAnswer;
+
+  // Konstruktor untuk menginisialisasi nilai dari questionText dan correctAnswer.
+  Question({
