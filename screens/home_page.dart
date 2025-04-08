2f367bad2bbbc0e876a48ce52e3fb74f89fd3d01
Aditya Eka Febriano <149566858+23091397048AdityaEkaFebriano@users.noreply.github.com>
2025-04-08 20:30:42 +0700

    Update home_page.dart

--- screens/home_page.dart
+++ screens/home_page.dart
@@ -1,1 +1,30 @@
+// Import library Flutter dan halaman lainnya
+import 'package:flutter/material.dart';
+import '../models/quiz_result.dart';
+import 'quiz_page.dart';
+import 'result_history_page.dart';
 
+// Widget halaman utama aplikasi
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // State untuk HomePage
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Menyimpan riwayat hasil kuis
+  List<QuizResult> quizHistory = [];
+
+  // Menambahkan hasil kuis ke daftar riwayat
+  void addQuizResult(QuizResult result) {
+    setState(() {
+      quizHistory.add(result);
+    });
+  }
+
+  // Build UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
