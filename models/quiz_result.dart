7f73bf3c9b0283fee80a047ace396db0bd7e115f
Aditya Eka Febriano <149566858+23091397048AdityaEkaFebriano@users.noreply.github.com>
2025-04-08 20:29:57 +0700

    Update quiz_result.dart

--- models/quiz_result.dart
+++ models/quiz_result.dart
@@ -1,1 +1,6 @@
+// Kelas ini merepresentasikan hasil dari sebuah kuis.
+class QuizResult {
+  // Tanggal dan waktu saat kuis diselesaikan.
+  final DateTime date;
 
+  // Total jumlah pertanyaan dalam kuis.
