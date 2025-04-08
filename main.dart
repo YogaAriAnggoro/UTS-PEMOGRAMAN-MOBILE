7ddc03f6e39fcaff867391e6eb7c501aa8037f9c
Aditya Eka Febriano <149566858+23091397048AdityaEkaFebriano@users.noreply.github.com>
2025-04-08 20:28:38 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,8 @@
+// Import package Flutter untuk menggunakan widget dasar seperti MaterialApp, ThemeData, dll.
+import 'package:flutter/material.dart';
 
+// Import file HomePage dari folder screens
+import 'screens/home_page.dart';
+
+// Fungsi utama yang akan dijalankan pertama kali saat aplikasi dimulai
+void main() => runApp(QuizzIn()); // Menjalankan aplikasi dan memuat widget QuizzIn
